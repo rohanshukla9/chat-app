568e13563dc4b7136940e75d75b1e3cad69813fb
unknown <voidrohan@gmail.com>
2020-11-30 18:31:14 +0530

    Initial websocket setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import express from 'express'
-import path from 'path'
+const express = require('express')
+const http = require('http')
+const path = require('path')
+const socketio = require('socket.io')
 
 
 const app = express()
+const server = http.createServer(app)
 
-const __dirname = path.resolve()
-const directory = path.join(__dirname, '/public')
+const io = socketio(server)
+
+
+//const __dirname = path.resolve()
+const directory = path.join(__dirname, '../public')
 
 app.use(express.static(directory))
 
-//app.get('/', )
+io.on('connection', () => {
+  console.log('New websocket connection')
+})
 
-app.listen(
+server.listen(
   3000,
   console.log(
     `Server running on port 3000`
